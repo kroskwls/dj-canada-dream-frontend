5be767edc7303a32f464df81d7b60add2904f73a
kroskwls <kroskwls@gmail.com>
2022-05-20 16:03:52 +0900

    Update apollo.ts

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const uri = `${process.env.REACT_APP_SERVER_URI}/graphql`;
+console.log(uri);
 export const client = new ApolloClient({
 	uri, cache: new InMemoryCache()
 });
